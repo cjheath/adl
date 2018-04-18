d47cc21c401185ef9224bb49d7d01b1bb50196fe
Clifford Heath <clifford.heath@gmail.com>
2018-04-18 20:59:00 +1000

    Process most assignments, except to inherited variables

--- adl.rb
+++ adl.rb
@@ -76,7 +76,7 @@ class ADL
       @parent.pathname+'.'+@variable.name+(@is_final ? '=' : '~=') + @value.inspect
     end
 
-    def inline
+    def inline level = ''
       %Q{#{@is_final ? ' =' : ' ~='} #{
         if ADLObject === @value
           @value.pathname
@@ -98,7 +98,7 @@ class ADL
     end
 
     def emit level = ''
-      puts "#{level}#{@variable.name}#{inline};"
+      puts "#{level}#{@variable.name}#{inline level};"
     end
   end
 
@@ -117,6 +117,7 @@ class ADL
     @string.syntax = /'(\\[befntr']|\\[0-7][0-7][0-7]|\\0|\\x[0-9A-F][0-9A-F]|\\u[0-9A-F][0-9A-F][0-9A-F][0-9A-F]|[^\\'])*'/;
     @enumeration = ADLObject.new(@builtin, 'Enumeration', @object, nil)
     @boolean = ADLObject.new(@builtin, 'Boolean', @enumeration, nil)
+    @is_sterile = ADLObject.new(@object, 'Is Sterile', @boolean, nil)
     @false = ADLObject.new(@builtin, 'False', @boolean, nil)
     @true = ADLObject.new(@builtin, 'True', @boolean, nil)
 
@@ -166,7 +167,8 @@ class ADL
     end
     return o if path_name.empty?
 
-    # Ascend the parent chain until we fail or find our name:
+    # Ascend the parent chain until we fail or find our first name:
+    # REVISIT: If we descend a supertype's child, this may become contextual!
     until no_ascend or path_name.empty? or m = o.member?(path_name[0])
       o = o.parent    # Ascend
       error("Failed to find #{path_name[0].inspect} from #{@stack.last.name}") unless o
@@ -175,7 +177,8 @@ class ADL
     path_name.shift
     return o if path_name.empty?
 
-    # Now descend from the 
+    # Now descend from the current position down the named children
+    # REVISIT: If we descend a supertype's child, this becomes contextual!
     path_name.each do |n|
       m = o.member?(n)
       error("Failed to find #{n.inspect} in #{o.pathname}") unless m
@@ -314,19 +317,28 @@ class ADL
     end
 
     def definition(object_name)
-      # print "In #{@adl.stack.last.inspect} looking at "; p peek; debugger
+      # print "In #{@adl.stack.last.inspect} defining #{object_name} and looking at "; p peek; debugger
       unless defining = reference(object_name) || alias_from(object_name)
+        inheriting = peek('inherits')
         supertype_name = type
-        defining = @adl.start_object(object_name, supertype_name)
-        is_block = block
-        is_array = array_indicator
-        is_assign = assignment(defining.parent, defining) # REVISIT: Check parameters
-        if !is_block || is_array || is_assign
+
+        # We only want to start a new object if there's a supertype, or a block, or an array indicator
+        if supertype_name || peek('open') || peek('lbrack') || inheriting
+          defining = @adl.start_object(object_name, supertype_name)
+          is_block = block object_name
+          is_array = array_indicator
+          @adl.end_object
+          assignment(defining)
+        elsif object_name
+          # print "In #{@adl.stack.last.inspect} defining #{object_name} and looking at "; p peek
+          variable = @adl.resolve_name(object_name)
+          assigned = assignment(variable)
+        end
+        if !is_block || is_array || assigned
           peek 'close' or require 'semi'
         end
       end
       opt_white
-      @adl.end_object
       true
     end
 
@@ -335,11 +347,11 @@ class ADL
         opt_white
         supertype_name = path_name
         error('Expected supertype, body or ;') unless supertype_name or peek('semi') or peek('open') or peek('approx') or peek('equals')
-        supertype_name
+        supertype_name || ['Object']
       end
     end
 
-    def block
+    def block object_name
       if has_block = expect('open')
         opt_white
         @adl.start_block
@@ -367,15 +379,19 @@ class ADL
         reference_to = path_name
         error('expected path name for Reference') unless reference_to
 
+        # Create the reference
         # An eponymous reference uses reference_to for object_name
         defining = @adl.start_object(object_name||reference_to, ['Reference'])
 
-        # Create the reference:
+        # Find what this is a reference to, and add a final assignment (to itself) for its type:
         reference_object = @adl.resolve_name(reference_to)
         defining.assign(defining, reference_object, true)
-        is_block = block
-        # REVISIT: The following tentative assignment has the same parent as the final assignment above. Perhaps that assignment should be on `defining` not its parent?
-        is_assigned = assignment(defining.parent, defining)
+
+        is_block = block object_name
+        @adl.end_object
+
+        # The following assignment has the same parent as the Reference itself
+        assignment(defining)
         peek('rbrack') or require 'semi' if !is_block || is_assigned
         opt_white
         defining
@@ -386,18 +402,24 @@ class ADL
       return nil unless expect('rename')
       defining = @adl.start_object(object_name, ['Alias'])
       defining.assign(defining, @alias_for, false)
+      @adl.end_object
       defining
     end
 
-    def assignment parent, variable
-      if operator = (expect('equals') || expect('approx'))
+    def assignment variable
+      if operator = ((is_final = expect('equals')) || expect('approx'))
         opt_white
+
+        # REVISIT: Detect the required value type here, including arrays, and deal with it
         val = value
+
+        parent = @adl.stack.last
+        parent.assign variable, val, is_final
       end
     end
 
     def value
-      integer or string or path_name or array or expect('regexp')
+      integer or string or (p = path_name and @adl.resolve_name(p)) or array or expect('regexp')
     end
 
     def integer
